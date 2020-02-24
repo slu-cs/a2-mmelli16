1d76347ebec05530adeb937c23bcad55772d0a1a
Matthew Ellis <mmelli16@stlawu.edu>
2020-02-24 10:14:08 -0500

    create3

--- create.js
+++ create.js
@@ -5,6 +5,26 @@ const file = readline.createInterface({
 });
 const mongoose = require('mongoose');
 const connect = require('./db');
-const registeredVoter= require('./schema');
+const voter= require('./schema');
 
 connect();
+
+const voters=[];
+file.on('line', function(line) {
+  const columns = line.split(',');
+  voters.push(
+    new voter ({
+    firstName: columns[0],
+    last_n: columns[1],
+    zipcode: columns[2],
+    history: columns[3]})
+  );
+});
+
+file.on('close', function(){
+  mongoose.connection.dropDatabase()
+    .then(()=> Promise.all(voters.map(v=>v.save())))
+    .then(() => mongoose.connection.close())
+    .then(()) => console.log("File Is Ready"))
+    .catch(error => console.error(error.stack));
+});
